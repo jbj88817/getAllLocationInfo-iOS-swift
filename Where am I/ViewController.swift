8ec9af5f5d32d8bc7f4ea245fb2fd8d01b55e871
Bojie Jiang <jiangbojie@gmail.com>
2015-08-04 15:45:03 -0700

    Initial Commit

---
+++ Where am I/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Where am I
+//
+//  Created by Bojie Jiang on 8/4/15.
+//  Copyright © 2015 Bojie Jiang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
